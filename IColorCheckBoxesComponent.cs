ece66df1d56749d814eceaac8089370dd7146f70
Heinrich Braasch <hbraasch@users.noreply.github.com>
2026-01-07 07:57:38 +1000

    Add IColorCheckBoxesComponent interface for color checkboxes
    Introduced the internal IColorCheckBoxesComponent interface in the NamedMetaTagManager namespace. This interface provides methods to initialize, update, and query color-based checkboxes, as well as an async callback for checkbox state changes. Uses System.Drawing.Color and supports asynchronous event handling.

---
+++ IColorCheckBoxesComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedMetaTagManager
+{
+    internal interface IColorCheckBoxesComponent
+    {
+        /// <summary>
+        /// Used to set amount of checkboxes, their colours and their checked status
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <param name="isChecked"></param>
+        public void Init(List<Color> colors, List<bool> isChecked);
+        /// <summary>
+        /// Used to update amount of checkboxes, their colours and their checked status
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <param name="isChecked"></param>
+        public void Update(List<Color> colors, List<bool> isChecked);
+        /// <summary>
+        /// Used to determine which checkboxes are checked
+        /// </summary>
+        /// <returns></returns>
+        public List<Color> GetCheckedColors();
+        /// <summary>
+        /// Callback which triggers when one of the checkboxes change
+        /// </summary>
+        /// <returns>
+        ///     Color: To indicate which checkbox changed
+        ///     bool: To indicate the final checked state of the checkbox
+        /// </returns>
+        public Task<Action<Color, bool>> CheckboxChanged();
+    }
+}
